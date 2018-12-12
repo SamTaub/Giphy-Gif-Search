e9d8a497a9aaa65d51dd204cc0d780a8ae64b38d
Sam Taub <staub91@gmail.com>
2018-12-12 18:14:37 -0500

    page will now properly empty old gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,7 +20,9 @@ $("#submit-button").on("click", function(){
 });
 
 function createGif (){
-
+    
+    $("#gif-container").empty();
+    
     let gif = $(this).attr("data-name");
     let apiKey = "MVW6VrC0Lj9n9Rp8wg8TOVl2We174UvH";
     let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=" + apiKey + "&limit=10";
@@ -42,8 +44,6 @@ function createGif (){
         $("#gif-container").prepend(getGif);
         $("#gif-container").prepend(rating);
         };
-
-
     });
 };
 
